ad09e7fea41e42ea7c453dbc9888bc31fc3bab55
3oL1v <3oliv93212@gmail.com>
2026-03-19 18:18:49 +0300

    Shorten Alembic revision id for Postgres

--- backend/alembic/versions/0002_today_slots_hide.py
+++ backend/alembic/versions/0002_today_slots_hide.py
@@ -4,7 +4,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0002_today_slots_and_completed_hide"
+revision = "0002_today_slots_hide"
 down_revision = "0001_mvp_schema"
 branch_labels = None
 depends_on = None
